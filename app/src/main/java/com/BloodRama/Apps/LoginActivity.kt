905ea8e9e959160e9926cab52d7516a31dd0b928
unknown <Iqbal Putra Ramadhan>
2022-06-10 10:42:54 +0700

    firstcommit

--- app/src/main/java/com/BloodRama/Apps/LoginActivity.kt
+++ app/src/main/java/com/BloodRama/Apps/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.BloodRama.Apps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val button = findViewById<Button>(R.id.button_login)
+        button.setOnClickListener{
+            val Intent = Intent(this,HomePage::class.java)
+            startActivity(Intent)
+        }
     }
 }
